8fbff1a7e53641f05cb2a84f1c4499b205f396b0
Adrien ROBION <arobion@e3r10p18.42.fr>
2019-03-13 18:52:30 +0100

    interactive bonus OK

--- main.py
+++ main.py
@@ -4,14 +4,7 @@ from xs_lexer import Lexer
 from xs_setting import Setting
 
 
-def main():
-    setting = Setting()
-    if setting.graph_rules:
-        try:
-            print('Importing visualizer (this may take some time) ...')
-            from xs_visualizer import Visualizer
-        except ImportError:
-            raise ImportError('Failed to import visualizer')
+def process_engine(setting):
     try:
         graph = Graph()
         for rule in setting.rules:
@@ -38,6 +31,32 @@ def main():
     except Exception as e:
         print(e)
 
+def ask_new_facts(setting):
+    entry = input("Previous facts where : {}\nEnter the new facts please ('x' to quit)\n".format(setting.true_facts))
+    if entry == "x":
+        print("Good bye")
+        setting.interactive = False
+        return
+    for l in entry:
+        if ord(l) < 65 or ord(l) > 90:
+            print("Please enter only valid facts")
+            ask_new_facts(setting)
+    setting.true_facts = entry
+
+def main():
+    setting = Setting()
+    if setting.graph_rules:
+        try:
+            print('Importing visualizer (this may take some time) ...')
+            from xs_visualizer import Visualizer
+        except ImportError:
+            raise ImportError('Failed to import visualizer')
+    if setting.interactive:
+        while setting.interactive:
+            process_engine(setting)
+            ask_new_facts(setting)
+    else:
+        process_engine(setting)
 
 if __name__ == "__main__":
     main()
